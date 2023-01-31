330c5e189636c74f4ef3c4683e82bbd48b2900f9
UO281827 <UO281827@uniovi.es>
2023-01-31 09:57:33 +0000

    Auto update

---
+++ 01/sizeof/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (int argc, char** argv) 
+{
+	char 	c = 'a';
+	int	i = 0;
+	double	z = 0.0;
+	printf("tipo;operacion;resultado\n");
+
+	printf("char;sizeof(c);%lu\n", sizeof(c));
+	printf("int;sizeof(i);%lu\n", sizeof(i));
+	printf("double;sizeof(z);%lu\n", sizeof(z));
+
+	printf("char;&c;%p\n", &c);
+	printf("int;&i;%p\n", &i);
+	printf("double;&z;%p\n", &z);
+
+	printf("char;sizeof(&c);%lu\n", sizeof(&c));
+	printf("int;sizeof(&i);%lu\n", sizeof(&i));
+	printf("double;sizeof(&z);%lu\n", sizeof(&z));
+
+	return 0;
+}
