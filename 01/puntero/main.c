8071bef03ab69183b5d93b4806cd8e3f879c0390
UO281827 <UO281827@uniovi.es>
2023-01-31 09:57:36 +0000

    Auto update

---
+++ 01/puntero/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char** argv)
+{
+	int n = 25;
+	printf("La variable n vale: %d\n", n);
+	printf("La direccion de memoria de la variable n es %p\n", &n);
+	return 0;
+}
